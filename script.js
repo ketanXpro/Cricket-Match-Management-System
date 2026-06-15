d56d3ca302e9c14143ced087311aa9e0edc245b9
Ketan Kumbhar <157051622+ketanXpro@users.noreply.github.com>
2026-06-15 11:46:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,8 @@
-let score = 0;
-let wickets = 0;
-let balls = 0;
+let match = JSON.parse(localStorage.getItem("currentMatch"));
 
-function updateScore() {
-  let overs = Math.floor(balls / 6) + "." + (balls % 6);
-
-  document.getElementById("scoreDisplay").innerHTML =
-    score + "/" + wickets + " (" + overs + ")";
+if (!match) {
+    window.location.href = "schedule.html";
 }
 
-function addRun(run) {
-  score += run;
-  balls++;
-  updateScore();
-}
-
-function wicket() {
-  wickets++;
-  balls++;
-  updateScore();
-}
-
-function extraRun(type) {
-  score++;
-
-  updateScore();
-}
-
-function startMatch() {
-  let team1 = document.getElementById("team1").value;
-  let team2 = document.getElementById("team2").value;
-  let overs = document.getElementById("overs").value;
-
-  localStorage.setItem("team1", team1);
-  localStorage.setItem("team2", team2);
-  localStorage.setItem("overs", overs);
-
-  window.location.href = "scoreboard.html";
-}
+const team1ScoreEl = document.getElementById("team1Score");
+const team2ScoreEl =
